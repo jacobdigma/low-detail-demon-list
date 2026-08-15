6d92a8e56f01dbda17fe03c28127cc00ac7aa6da
jacobdigma <guyr0820@gmail.com>
2026-08-15 16:57:20 +0200

    Update pack name and levels in Packs.js

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -28,25 +28,11 @@ export default {
             listData: [],
             packs: [
                 {
-                    name: "Neptune pack 1",
+                    name: "gilbert pack 1",
                     color: "#CD7F32",
-                    levels: ["xStep V2", "Clutterfunk V2", "Electroman Adventures V2"]
-                },
-                {
-                    name: "Digma Pack",
-                    color: "#C0C0C0",
-                    levels: ["m tolot", "Speed Racer", "Blackfire Backfire"]
-                },
-                {
-                    name: "Noument Pack",
-                    color: "#FFD700",
-                    levels: ["Noument", "Unnerfed noument", "Hellishment"]
-                },
-                {
-                    name: "RobTop Pack",
-                    color: "#0096FF",
-                    levels: ["Deadlocked", "Theory of Everything 2", "Clubstep"]
+                    levels: ["bg V2", "g V2", "Electroman g V2"]
                 }
+        
             ]
         };
     },
